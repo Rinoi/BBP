7ef189a909da10c4763b5b6650699e33c8093ef3
mayol_l <lucas.mayol@epitech.eu>
2024-01-26 15:25:46 +0100

    update devis test

--- script/testDevis.sh
+++ script/testDevis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X GET -H "Content-Type: application/json" -d '{"stocks": [{"beerId": 1, "quantity": 4}, {"beerId": 2, "quantity": 7}]}' http://localhost:7042/api/wholesaler/1/devis
+curl -X GET -H "Content-Type: application/json" -d '{"stocks": [{"beerId": 3, "quantity": 100}, {"beerId": 3, "quantity": 0}]}' http://localhost:7042/api/wholesaler/2/devis
